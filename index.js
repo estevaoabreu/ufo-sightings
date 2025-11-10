300c75c2f3ff2be8197417ab6d6efe7d98f9a2c1
Estêvão Abreu <estevao.abreu40@gmail.com>
2025-11-10 11:49:19 +0000

    Test

---
+++ index.js
@@ -0,0 +1,12 @@
+path = ƒ(t)
+path = d3.geoPath()
+
+us = Object {
+  type: "Topology"
+  bbox: Array(4) [-57.66491068874468, 12.97635452036684, 957.5235629133763, 606.5694262668667]
+  transform: Object {scale: Array(2), translate: Array(2)}
+  objects: Object {counties: Object, states: Object, nation: Object}
+  arcs: Array(9462) [Array(2), Array(3), Array(2), Array(24), Array(2), Array(11), Array(13), Array(32), Array(2), Array(2), Array(6), Array(5), Array(3), Array(5), Array(2), Array(2), Array(2), Array(5), Array(6), Array(2), …]
+}
+
+us = FileAttachment("counties-albers-10m.json").json()
